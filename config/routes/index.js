e348a609aa00e3b84284fe7262961a9a5546e93f
Abdulsemiu Atanda <semiu@live.com>
2018-06-13 16:00:28 +0100

    add HomeTabs

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,13 +1,59 @@
 import React from 'react'
 import {Platform} from 'react-native'
-import {createStackNavigator, createBottomTabNavigator} from 'react-navigation'
+import {createStackNavigator, createTabNavigator} from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import Home from '../../components/Home.react'
 import Tweets from '../../components/Tweets.react'
+import Search from '../../components/Search.react'
+
+const HomeTabs = createTabNavigator({
+  Trends: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: "Trends",
+      tabBarIcon: ({ tintColor }) =>
+        <Icon
+          name={Platform.OS === "ios" ? "ios-trending-up" : "md-trending-up"}
+          size={32} color={tintColor}
+        />
+    }
+  },
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      tabBarLabel: "Search",
+      tabBarIcon: ({ tintColor }) =>
+        <Icon
+          name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+          size={32}
+          color={tintColor}
+        />
+    }
+  }
+},
+{
+  swipeEnabled: true,
+  tabBarOptions: {
+    activeTintColor: "black",
+    inactiveTintColor: "#A9A9A9",
+    showIcon: true,
+    style: {
+      backgroundColor: "#F7F7F7"
+    },
+    indicatorStyle: {
+      opacity: 0
+    },
+    tabStyle: {
+      height: 40
+    },
+    showLabel: false
+  }
+})
 
 const AppNavigator = createStackNavigator({
   Home: {
-    screen: Home,
+    screen: HomeTabs,
     navigationOptions: {
       title: 'Trends'
     }
