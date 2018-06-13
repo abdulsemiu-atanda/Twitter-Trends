a929ba0a69b32f8283ffd9fec9bd1632bc291f69
Abdulsemiu Atanda <semiu@live.com>
2018-06-13 14:16:11 +0100

    adjust Tweets component styles

--- components/Tweets.react.js
+++ components/Tweets.react.js
@@ -40,7 +40,6 @@ class Tweets extends Component {
   }
 
   renderRow(tweet) {
-    console.log(tweet.user)
     return (
       <View style={styles.card}>
         <View style={styles.cardHeader}>
@@ -51,7 +50,7 @@ class Tweets extends Component {
           </View>
         </View>
         <Text>{tweet.text}</Text>
-        {/* {tweet.media.length && <Image source={{uri: tweet.medis[0].media_url_https}} />} */}
+        {tweet.extended_entities && tweet.extended_entities.media.length && <Image style={styles.image} source={{uri: tweet.extended_entities.media[0].media_url_https}} />}
       </View>
     )
   }
@@ -82,12 +81,13 @@ const mapDispatchToProps = dispatch => ({
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#FFFF',
-    height: height * 0.28,
-    marginBottom: height * 0.01
+    justifyContent: 'flex-start',
+    marginBottom: height * 0.01,
+    width: width * 0.95,
+    marginLeft: width * 0.02
   },
   cardHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    flexDirection: 'row'
   },
   container: {
     flex: 1,
@@ -101,10 +101,15 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 15
   },
+  image: {
+    width: width * 0.95,
+    height: Math.round(width * 9 / 16)
+  },
   thumbnail: {
     width: (width * 0.15),
     height: (width * 0.15),
-    borderRadius: (width * 0.15) / 2
+    borderRadius: (width * 0.15) / 2,
+    marginRight: width * 0.05
   }
 })
 
